a12829eb8e35bb8e71847373b8ed05685869b07e
alonsodelcordova <67562587+alonsodelcordova@users.noreply.github.com>
2023-04-18 17:32:39 -0500

    sreen time and inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
+import moment = require('moment/moment');
 import * as React from 'react';
+import { useState } from 'react';
 import './style.css';
 
 export default function App() {
+  const [timeDate, setTimeDate] = useState(new Date());
+  const [alarms, setAlarms] = useState([]);
+
+  setTimeout(() => {
+    setTimeDate(new Date());
+  }, 1000);
+
   return (
     <div className="content">
-      <h1>App de Alarma</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <div className="card">
+        <h1 className="center">App de Alarma</h1>
+        <h6 className="center">{moment(timeDate).format('hh:mm:ss')}</h6>
+
+        <div className="form-group">
+          <input type="number" />
+          <span className="stag">:</span>
+          <input type="number" />
+        </div>
+        <div className="center">
+          <button>Add Alarm</button>
+        </div>
+
+        <p>Start editing to see some magic happen :)</p>
+      </div>
     </div>
   );
 }
