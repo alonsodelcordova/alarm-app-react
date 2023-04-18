601706aee79ce17746c2726ab010efb5c17d4978
alonsodelcordova <67562587+alonsodelcordova@users.noreply.github.com>
2023-04-18 16:46:04 -0500

    update prueba

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './style.css';
 
 export default function App() {
   return (
-    <div>
+    <div className="content">
       <h1>App de Alarma</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
